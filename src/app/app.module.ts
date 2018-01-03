7f1165fd31f50983c2019ae138791e3e4e53e5a2
unknown <ravireshmi2@gmail.com>
2018-01-03 22:06:54 +1300

    Initial Commit - Tutorial 1 - CSS Change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 
+// this guy below describes the class that follows it
+// e.g. decorator @NgModule()
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [ AppComponent ],
